47f25212b6a49ec4be4a32594fe650ecd168d143
Jeremiah Brints <timbothyaracnio@gmail.com>
2019-10-07 15:39:41 -0500

    part 3 for todays learning

--- strign n' tecst.py
+++ strign n' tecst.py
@@ -20,3 +20,28 @@ w = "This is the left side of..."
 e = "a string with a right side."
 
 print(w + e)
+
+# more printing funn
+print("Mary had a little lamb,")
+print("little lamb,")
+print("LITTLE LAMB!")
+print("Mary had a little lamb,")
+print("It's fleece was white as %s." % 'snow')
+print("and everywhere that Mary went,")
+print("Mary went,")
+print("MARY WENT!")
+print("everywhere that Mary went,")
+print("." * 10)
+
+end1 = "H"
+end2 = "a"
+end3 = "m"
+end4 = "b"
+end5 = "u"
+end6 = "r"
+end7 = "g"
+end8 = "e"
+end9 = "r"
+
+print(end1 + end2 + end3,)
+print(end4 + end5 + end6 + end7 + end8 + end9)
