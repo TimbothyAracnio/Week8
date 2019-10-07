b80cbd1df4a98745cfa902437cf9b25195f23f38
Jeremiah Brints <timbothyaracnio@gmail.com>
2019-10-07 15:53:29 -0500

    commenting on everything

--- strign n' tecst.py
+++ strign n' tecst.py
@@ -1,27 +1,44 @@
 # more strings and text
 
+# this is a variable that equals a string with number attached
 x = "There are %d types of people" % 3
 
+# this is a variable that is assigned to a string
 goodAtMath = "good at math"
+# this is a variable that is assigned to a string
 badAtMath = "not"
+
+# this is a variable that equals a string with two other variables
 y = "Those who are %s and those who are %s" % (goodAtMath, badAtMath)
 
+# this prints a string
 print(x)
+# this prints a string
 print(y)
 
+# this prints a string with a variable
 print("I said: %r.:" % x)
+# this prints a string with a variable
 print("I also said: '%s'." % y)
 
+
+# this is a boolean variable
 hilarious = True
+# this is a variable that is assigned to a string
 jokeEvaluation = "Isn't that joke so funny?! %r"
 print(jokeEvaluation % hilarious)
 
+# this is a variable that is assigned to a string
 w = "This is the left side of..."
+# this is a variable that is assigned to a string
 e = "a string with a right side."
 
+# this prints two strings attached to each other
 print(w + e)
 
 # more printing funn
+
+# these print strings
 print("Mary had a little lamb,")
 print("little lamb,")
 print("LITTLE LAMB!")
@@ -31,8 +48,11 @@ print("and everywhere that Mary went,")
 print("Mary went,")
 print("MARY WENT!")
 print("everywhere that Mary went,")
+
+# this prints strings times a specific multiplier
 print("." * 10)
 
+# SEVERAL variables that are assigned to strings
 end1 = "H"
 end2 = "a"
 end3 = "m"
@@ -43,5 +63,6 @@ end7 = "g"
 end8 = "e"
 end9 = "r"
 
+# these print several strings attached to each other
 print(end1 + end2 + end3,)
 print(end4 + end5 + end6 + end7 + end8 + end9)
