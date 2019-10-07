a5041ec292b79d571ba5e04b1b6db26270cea2fb
Jeremiah Brints <timbothyaracnio@gmail.com>
2019-10-07 15:28:01 -0500

    part 2

--- strign n' tecst.py
+++ strign n' tecst.py
@@ -5,3 +5,18 @@ x = "There are %d types of people" % 3
 goodAtMath = "good at math"
 badAtMath = "not"
 y = "Those who are %s and those who are %s" % (goodAtMath, badAtMath)
+
+print(x)
+print(y)
+
+print("I said: %r.:" % x)
+print("I also said: '%s'." % y)
+
+hilarious = True
+jokeEvaluation = "Isn't that joke so funny?! %r"
+print(jokeEvaluation % hilarious)
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print(w + e)
