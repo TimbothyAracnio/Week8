f14246315b991ea78b62df1956d12c4f83cab205
Jeremiah Brints <timbothyaracnio@gmail.com>
2019-10-07 15:19:14 -0500

    made new file and am at part 1

---
+++ strign n' tecst.py
@@ -0,0 +1,7 @@
+# more strings and text
+
+x = "There are %d types of people" % 3
+
+goodAtMath = "good at math"
+badAtMath = "not"
+y = "Those who are %s and those who are %s" % (goodAtMath, badAtMath)
